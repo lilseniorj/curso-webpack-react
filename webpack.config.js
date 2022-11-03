861cd33bfea3f01048260ca321a470c973fb6be5
Jesus Vargas <jesusvarguer18@gmail.com>
2022-11-03 15:47:45 -0500

    Html loader

--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,7 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.(js|jsx)$/,
+        test: /\.js$|jsx/,
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
